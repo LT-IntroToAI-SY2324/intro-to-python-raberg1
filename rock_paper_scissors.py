c126a21193c6d8b672f5ce7f30f0448a3697ddb9
Robert Berg <raberg1@gmail.com>
2023-09-06 11:02:54 -0500

    started rock paper scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,17 @@
 # We will write a rock paper scissors game in class - Complete it in this file
+import random
+
+player_choice = "rock"
+computer_choice = "paper"
+
+# Create a function that gets the choices
+def get_choices():
+    options = ["rock", "paper", "scissors"]
+    player_choice = "scissors"
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+
+    return choices
+
+choices = get_choices()
+print(choices)
